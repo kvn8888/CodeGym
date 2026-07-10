da9959eba6d9d6f054f24328518731076b582a2b
Kevin Chen <kvn.c8888@gmail.com>
2026-07-10 15:54:40 -0400

    Remove SettingsPage route from App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,6 @@ import { GeneratePage } from './features/generate/GeneratePage';
 import { DashboardPage } from './features/dashboard/DashboardPage';
 import { MarathonPage } from './features/marathon/MarathonPage';
 import { MemoryPage } from './features/memory/MemoryPage';
-import { SettingsPage } from './features/settings/SettingsPage';
 
 function App() {
   return (
@@ -23,7 +22,6 @@ function App() {
               <Route path="/marathon" element={<MarathonPage />} />
               <Route path="/memory" element={<MemoryPage />} />
               <Route path="/dashboard" element={<DashboardPage />} />
-              <Route path="/settings" element={<SettingsPage />} />
             </Route>
           </Routes>
         </BrowserRouter>
